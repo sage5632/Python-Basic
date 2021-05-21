27ec3bcd964a35ca0375b970f5d5fb5ec358d041
parkjh <sage5632@gmail.com>
2021-05-21 17:45:27 +0900

    문제풀이 01

--- quiz01.py
+++ quiz01.py
@@ -31,8 +31,16 @@ def quiz1():
 
 
 
-# def quiz2():
+def quiz2():
+
+    lst = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+    # 리스트 내부의 순서를 일부 뒤집기
+    print (lst[0], lst[1], lst[2], lst[6], lst[5], lst[4], lst[3], lst[7], lst[8], lst[9])
+
+    # 리스트로부터 4,5,6,7을 추출하여 slice에 담기
+
 
 if __name__ == "__main__":
-    quiz1()
-  #  quiz2()
+  # quiz1()
+    quiz2()
